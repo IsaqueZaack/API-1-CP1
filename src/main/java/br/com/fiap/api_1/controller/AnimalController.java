590f5499bc8766a2b34abdf626a59f3bab38ae72
Isaque Santana Paixão <isaquesantanapaixao444@gmail.com>
2026-05-04 16:15:57 -0300

    feat: inicio do CP2

--- src/main/java/br/com/fiap/api_1/controller/AnimalController.java
+++ src/main/java/br/com/fiap/api_1/controller/AnimalController.java
@@ -1,4 +1,4 @@
-package br.com.fiap.api_1;
+package br.com.fiap.api_1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/br/com/fiap/api_1/model/Animal.java
@@ -0,0 +1,12 @@
+package br.com.fiap.api_1.model;
+
+public class Animal {
+    
+    private Long id;
+    private String nome;
+    private String tipo;
+
+    public Animal() {
+
+    }
+}
